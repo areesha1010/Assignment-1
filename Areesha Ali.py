3491318a480adef566226e36b632d5cdb9d73b6a
areesha1010 <m.ali78621649@gmail.com>
2024-10-30 11:46:00 +0300

    Add files via upload

---
+++ Areesha Ali.py
@@ -0,0 +1,20 @@
+num1=54
+num2=34
+num3=98
+sum=num1+num2+num3
+print(sum)
+diff=num1-num2-num3
+print(diff)
+product=num1*num2*num3
+print(product)
+average=(num1+num2+num3)/3
+print(average)
+# area of triangle
+area=23*33/2
+print(area)
+# area of sqaure
+area=40*40
+print(area)
+# area of circle
+area=3.14*55*55
+print(area)
